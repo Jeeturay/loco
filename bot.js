6bd4e61132c1543c1b9718fa94423ec441dfe0d6
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2018-12-16 18:23:29 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "508868117661745153" && message.channel.id === "523842486330720266"){
+
         if(msg.startsWith('>', 0)){
             if(msg === prefix + "bang"){
                 message.channel.send('BOOM!!!!!')
