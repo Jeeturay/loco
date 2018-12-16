a1eeb80b170ba00edbddc279d0da89b6c5469f4d
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2018-12-16 18:29:11 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,10 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "381443488392413184" && message.channel.id === "381397780385628181"){
+    if(message.author.id != "508868117661745153" && message.channel.id === "523846074507460610"){
         if(msg.startsWith('>', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "381397780385628181"){
-                message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
+            if(msg === prefix + "points" && message.channel.id === "523846074507460610"){
+                message.channel.send('You have ```0``` point lefr...!') // Sends a message to the channel, with the content
             }
             else{
                 message.channel.send("Hey, I only understand >bang right now. More will be added when my creator is free. :) ")
