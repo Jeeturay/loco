b4767f2e816e686a71e8c4f19aa4235dceb5c908
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2018-12-22 19:50:19 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "508868117661745153" && message.channel.id === "523846074507460610"){
+    if(message.author.id != "508868117661745153" && message.channel.id === "525541987299098634"){
         if(msg.startsWith('>', 0)){
-            if(msg === prefix + "buy"&& message.channel.id === "523846074507460610"){
+            if(msg === prefix + "buy"&& message.channel.id === "525541987299098634"){
                 message.channel.send('Sahil rana & Ritesh loco life dm ......!!')
 
                                     
